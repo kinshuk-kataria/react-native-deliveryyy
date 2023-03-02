fa29193306310e45ea9da97bb759d6ece31bbd40
Kinshuk <katariakinshuk3@gmail.com>
2023-03-02 23:09:33 +0530

    first commit

---
+++ screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+export default HomeScreen;
