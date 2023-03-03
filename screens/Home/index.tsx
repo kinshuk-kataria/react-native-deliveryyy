ae94a7a30ea3c77c43e396df511d9b077a9c6eff
Kinshuk <katariakinshuk3@gmail.com>
2023-03-03 20:04:02 +0530

    update header

--- screens/Home/index.tsx
+++ screens/Home/index.tsx
@@ -1,11 +1,48 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, SafeAreaView, Image, TextInput } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import {
+  AdjustmentsVerticalIcon,
+  MagnifyingGlassIcon,
+  ChevronDownIcon,
+  UserIcon
+} from 'react-native-heroicons/outline';
 
 function HomeScreen() {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({ headerShown: false });
+  }, []);
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView className="bg-white ">
+      {/**Header */}
+      <View className="flex-row pb-3 pt-1 items-center  space-x-2 px-3">
+        <Image
+          source={require('../../assets/avatar.png')}
+          className="h-7 w-7 p-4 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver now</Text>
+          <Text className="font-bold text-xl">
+            Current Location <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+
+        <UserIcon size={35} />
+      </View>
+      <View className="flex-row items-center pb-2  pt-1 space-x-3 px-3">
+        <View className="flex-row flex-1 space-x-3 bg-gray-300 items-center p-2">
+          <MagnifyingGlassIcon color="gray" />
+          <TextInput
+            placeholder="Restaurants and cuisine"
+            keyboardType="default"
+          />
+        </View>
+        <AdjustmentsVerticalIcon color="00CCBB" />
+      </View>
+    </SafeAreaView>
   );
 }
 export default HomeScreen;
